0b877809257391a013f1519d1df8c7f3a36430f1
hanhanhuang <549551740@qq.com>
2017-03-22 16:15:06 +0800

    update

---
+++ app/src/main/java/com/hyh/httpframe/HttpClientUtil.java
@@ -0,0 +1,12 @@
+package com.hyh.httpframe;
+
+/**
+ * 作者：hyh on 2017/3/16 16:33
+ * github: https://github.com/hanhan1
+ * QQ:549551740
+ * 邮箱：hyh5878@163.com
+ * 作用：
+ */
+public class HttpClientUtil {
+
+}
---
+++ app/src/main/java/com/hyh/httpframe/RequestUtil.java
@@ -0,0 +1,11 @@
+package com.hyh.httpframe;
+
+/**
+ * 作者：hyh on 2017/3/16 16:39
+ * github: https://github.com/hanhan1
+ * QQ:549551740
+ * 邮箱：hyh5878@163.com
+ * 作用：
+ */
+public class RequestUtil {
+}
